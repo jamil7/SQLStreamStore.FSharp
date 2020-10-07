0718b88c49f68d0834513d745c6f32db649ff5e6
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-10-07 21:26:54 +0200

    Add two tests

---
+++ tests/AppendTests.fs
@@ -0,0 +1,53 @@
+module Tests
+
+open Expecto
+open SqlStreamStore.FSharp
+open SqlStreamStore.FSharp.Tests
+
+
+[<Tests>]
+let tests =
+    testList
+        "Append Tests"
+        [ testAsync "Should append one message to stream." {
+              let inMemStore = new SqlStreamStore.InMemoryStreamStore()
+
+              let appendStream: AppendStreamDetails =
+                  { streamName = "test"
+                    version = AppendVersion.NoStream }
+
+              let msg =
+                  { id = StreamMessageId.Auto
+                    type_ = "testing"
+                    jsonData = "{}"
+                    jsonMetadata = "{}" }
+
+              let! appendResult = Append.appendNewMessage inMemStore appendStream msg
+
+              ExpectExtra.equal 0 appendResult.CurrentVersion
+          }
+
+          testAsync "Should append a list of messages to stream." {
+              let inMemStore = new SqlStreamStore.InMemoryStreamStore()
+
+              let appendStream: AppendStreamDetails =
+                  { streamName = "test"
+                    version = AppendVersion.NoStream }
+
+              let msg1 =
+                  { id = StreamMessageId.Auto
+                    type_ = "testing"
+                    jsonData = "{}"
+                    jsonMetadata = "{}" }
+
+              let msg2 =
+                  { id = StreamMessageId.Auto
+                    type_ = "testing"
+                    jsonData = "{}"
+                    jsonMetadata = "{}" }
+
+              let msgList = [ msg1; msg2 ]
+
+              let! appendResult = Append.appendNewMessages inMemStore appendStream msgList
+              ExpectExtra.equal 1 appendResult.CurrentVersion
+          } ]
