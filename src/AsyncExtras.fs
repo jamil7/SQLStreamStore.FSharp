cc27d40f686ec6c9394ff424b91b9e5efc6be4fe
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-10-14 21:16:37 +0200

    Move AsyncExtras.fs content to ExceptionsHandler.fs

--- src/AsyncExtras.fs
+++
@@ -1,5 +0,0 @@
-module Async
-
-let bind f m = async.Bind(m, f)
-
-let map f m = m |> bind (f >> async.Return)
--- src/ExceptionsHandler.fs
+++ src/ExceptionsHandler.fs
@@ -1,8 +1,14 @@
-module ExceptionsHandler
+namespace SqlStreamStore.Fsharp
 
-let simpleExceptionHandler (op: Async<'res>): Async<Result<'res, string>> =
-        op
-        |> Async.Catch
-        |> Async.map (function
-            | Choice1Of2 response -> Ok response
-            | Choice2Of2 exn -> Error exn.Message)
+module Async =
+    let bind f m = async.Bind(m, f)
+    let map f m = m |> bind (f >> async.Return)
+
+module ExceptionsHandler =
+
+    let simpleExceptionHandler (op: Async<'res>): Async<Result<'res, string>> =
+            op
+            |> Async.Catch
+            |> Async.map (function
+                | Choice1Of2 response -> Ok response
+                | Choice2Of2 exn -> Error exn.Message)
