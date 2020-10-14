8273e436600acf4e739adfaba5b5a09d3ceeb5c6
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-10-14 21:01:25 +0200

    Handle exceptions thrown by reading and appending

---
+++ src/ExceptionHandler.fs
@@ -0,0 +1,8 @@
+module ExceptionHandler
+
+let simpleExceptionHandler (op: Async<'res>): Async<Result<'res, string>> =
+        op
+        |> Async.Catch
+        |> Async.map (function
+            | Choice1Of2 response -> Ok response
+            | Choice2Of2 exn -> Error exn.Message)
