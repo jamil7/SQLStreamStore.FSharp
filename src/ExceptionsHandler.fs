f0484e99307381d49668c78062baa25e83d5afdc
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-10-14 21:53:53 +0200

    Remove unneeded function

--- src/ExceptionsHandler.fs
+++ src/ExceptionsHandler.fs
@@ -1,14 +1,13 @@
 namespace SqlStreamStore.FSharp
 
 module Async =
-    let bind f m = async.Bind(m, f)
-    let map f m = m |> bind (f >> async.Return)
+    let map f m = async.Bind(m, (f >> async.Return))
 
 module ExceptionsHandler =
 
     let simpleExceptionHandler (op: Async<'res>): Async<Result<'res, string>> =
-            op
-            |> Async.Catch
-            |> Async.map (function
-                | Choice1Of2 response -> Ok response
-                | Choice2Of2 exn -> Error exn.Message)
+        op
+        |> Async.Catch
+        |> Async.map (function
+            | Choice1Of2 response -> Ok response
+            | Choice2Of2 exn -> Error exn.Message)
