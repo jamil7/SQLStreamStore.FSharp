05eed5f1e4220c63901111a0f9907f85b243927a
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-10-24 21:46:47 +0200

    Refactor some variable names to help readability

--- src/Async.fs
+++ src/Async.fs
@@ -7,27 +7,27 @@ module Async =
     let map f m = async.Bind(m, (f >> async.Return))
 
     let awaitTaskWithInnerException (task: Task<'T>): Async<'T> =
-        Async.FromContinuations(fun (suc, exn, _cln) ->
+        Async.FromContinuations(fun (success, exception', _cancellationToken) ->
             task.ContinueWith(fun (t: Task<'T>) ->
-                if t.IsFaulted
-                then if t.Exception.InnerExceptions.Count = 1 then
-                         exn t.Exception.InnerExceptions.[0]
-                     else
-                         exn t.Exception
-                elif t.IsCanceled
-                then exn (TaskCanceledException())
-                else suc t.Result)
+                if t.IsFaulted then
+                    if t.Exception.InnerExceptions.Count = 1
+                    then exception' t.Exception.InnerExceptions.[0]
+                    else exception' t.Exception
+                elif t.IsCanceled then
+                    exception' (TaskCanceledException())
+                else
+                    success t.Result)
             |> ignore)
 
     let awaitTaskWithInnerException' (task: Task): Async<unit> =
-        Async.FromContinuations(fun (suc, exn, _cln) ->
+        Async.FromContinuations(fun (success, exception', _cancellationToken) ->
             task.ContinueWith(fun (t: Task) ->
-                if t.IsFaulted
-                then if t.Exception.InnerExceptions.Count = 1 then
-                         exn t.Exception.InnerExceptions.[0]
-                     else
-                         exn t.Exception
-                elif t.IsCanceled
-                then exn (TaskCanceledException())
-                else suc ())
+                if t.IsFaulted then
+                    if t.Exception.InnerExceptions.Count = 1
+                    then exception' t.Exception.InnerExceptions.[0]
+                    else exception' t.Exception
+                elif t.IsCanceled then
+                    exception' (TaskCanceledException())
+                else
+                    success ())
             |> ignore)
