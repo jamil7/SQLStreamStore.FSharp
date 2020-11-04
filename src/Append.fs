d32cf1bd6b39d8deb6255a38b9d989be1383a949
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-11-04 20:54:33 +0100

    Add docs. Change exception type.

--- src/Append.fs
+++ src/Append.fs
@@ -3,18 +3,20 @@ namespace SqlStreamStore.FSharp
 open SqlStreamStore.Streams
 
 module Append =
+    /// Appends a new message to a given stream.
     let appendNewMessage (store: SqlStreamStore.IStreamStore)
                          (streamName: string)
                          (appendVersion: AppendVersion)
                          (messageDetails: MessageDetails)
-                         : Async<Result<AppendResult, string>> =
+                         : Async<Result<AppendResult, exn>> =
         AppendRaw.appendNewMessage store streamName appendVersion messageDetails
         |> ExceptionsHandler.asyncExceptionHandler 
 
+    /// Appends a list of messages to a given stream.
     let appendNewMessages (store: SqlStreamStore.IStreamStore)
                           (streamName: string)
                           (appendVersion: AppendVersion)
                           (messages: MessageDetails list)
-                          : Async<Result<AppendResult, string>> =
+                          : Async<Result<AppendResult, exn>> =
         AppendRaw.appendNewMessages store streamName appendVersion messages
         |> ExceptionsHandler.asyncExceptionHandler 
