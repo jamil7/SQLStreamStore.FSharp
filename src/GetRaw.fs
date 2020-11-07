8efb2cf699a7a75296532bd1504d1d1537c0c14b
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-11-07 16:45:35 +0100

    Add messagePayload, and messagesPayload to get jsonData from a message(s).

--- src/GetRaw.fs
+++ src/GetRaw.fs
@@ -3,6 +3,15 @@ namespace SqlStreamStore.FSharp
 open SqlStreamStore.Streams
 
 module GetRaw =
-    let messagePayload (message: StreamMessage) =
-        message.GetJsonData
+    let messagePayload (message: StreamMessage): Async<string> =
+        async {
+            return! message.GetJsonData()
+                    |> Async.awaitTaskWithInnerException
+        }
 
+    let messagesPayload (messages: StreamMessage list): Async<string list> =
+        messages
+        |> List.map messagePayload
+        |> Async.parallelCombine
+
+    
