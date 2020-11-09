9a094c7eb791b67bfe7b07c99efa62aaa122e56b
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-11-09 16:20:29 +0100

    Add comments. Refactor.

--- src/GetRaw.fs
+++ src/GetRaw.fs
@@ -3,15 +3,17 @@ namespace SqlStreamStore.FSharp
 open SqlStreamStore.Streams
 
 module GetRaw =
+    /// Get jsonData from a StreamMessage.
+    /// Can throw exceptions.
+    /// Not recommended to use. Refer to Get module's functions.
     let messagePayload (message: StreamMessage): Async<string> =
         async {
             return! message.GetJsonData()
                     |> Async.awaitTaskWithInnerException
         }
 
-    let messagesPayload (messages: StreamMessage list): Async<string list> =
-        messages
-        |> List.map messagePayload
-        |> Async.parallelCombine
-
-    
+    /// Get a list of jsonData from a list of StreamMessages.
+    /// Can throw exceptions for each message.
+    /// Not recommended to use. Refer to Get module's functions.
+    let messagesPayload: StreamMessage list -> Async<string list> =
+        List.map messagePayload >> Async.parallelCombine
