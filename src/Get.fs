a13e5cc096a1bc73b1b0279cce9090ba2220e111
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-11-09 16:16:01 +0100

    Add comments

--- src/Get.fs
+++ src/Get.fs
@@ -3,10 +3,12 @@ namespace SqlStreamStore.FSharp
 open SqlStreamStore.Streams
 
 module Get =
+    /// Get jsonData from a StreamMessage or an exception wrapped in a Result.
     let messagePayload (message: StreamMessage): Async<Result<string, exn>> =
         GetRaw.messagePayload message
         |> ExceptionsHandler.asyncExceptionHandler
 
+    /// Get a list of jsonData from a list of StreamMessages of a list of exceptions wrapped in a Result.
     let messagesPayload (messages: StreamMessage list) =
         messages
         |> List.map messagePayload
