d5019d86892a9964a219701c9a336c7d0c065d45
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2021-03-06 16:51:55 +0100

    serialization refactor

--- src/Infrastructure.fs
+++ src/Infrastructure.fs
@@ -5,36 +5,27 @@ module SerializationConfig =
     open System.Text.Json
     open System.Text.Json.Serialization
 
-    type JsonConfig<'a> =
-        {
-            encode : 'a -> string
-            decode : string -> 'a
-        }
+    type SerializerConfig<'a> =
+        { encode: 'a -> string
+          decode: string -> 'a }
 
     let private opt =
-        JsonSerializerOptions (IgnoreNullValues = true)
+        JsonSerializerOptions(IgnoreNullValues = true)
 
     let private converterOpt =
-        JsonFSharpConverter (
-            unionTagName = "tag",
-            unionFieldsName = "value",
-            unionEncoding = JsonUnionEncoding.AdjacentTag
-        )
+        JsonFSharpConverter
+            (unionTagName = "tag", unionFieldsName = "value", unionEncoding = JsonUnionEncoding.AdjacentTag)
 
     opt.Converters.Add converterOpt
 
-    let DefaultSerializationConfig : JsonConfig<'a> =
-        {
-            encode = fun (eventData : 'a) -> JsonSerializer.Serialize<'a> (eventData, opt)
-            decode = fun (data : string) -> JsonSerializer.Deserialize<'a> (data, opt)
-        }
+    let DefaultSerializationConfig: SerializerConfig<'a> =
+        { encode = fun (eventData: 'a) -> JsonSerializer.Serialize<'a>(eventData, opt)
+          decode = fun (data: string) -> JsonSerializer.Deserialize<'a>(data, opt) }
 
-module JayJson =
+module internal Serdes =
 
     open SerializationConfig
 
-    let encode<'a> : 'a -> string =
-        DefaultSerializationConfig.encode
+    let encode<'a> : 'a -> string = DefaultSerializationConfig.encode
 
-    let decode<'a> : string -> 'a =
-        DefaultSerializationConfig.decode
+    let decode<'a> : string -> 'a = DefaultSerializationConfig.decode
--- src/StreamEvent.fs
+++ src/StreamEvent.fs
@@ -74,12 +74,12 @@ type StreamEvent<'a> =
 module StreamEvent =
     let ofStreamMessage (msg: StreamMessage): StreamEvent<'a> =
         let meta =
-            JayJson.decode<Metadata> msg.JsonMetadata
+            Serdes.decode<Metadata> msg.JsonMetadata
 
         let getData () =
             asyncResult {
                 let! json = msg.GetJsonData()
-                return JayJson.decode<'a> json
+                return Serdes.decode<'a> json
             }
 
         { author = meta.author
