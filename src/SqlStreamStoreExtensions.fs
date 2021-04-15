20f17bca48824d94a3105c06a9f3ae0a05cc1391
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-04-15 22:55:00 +0200

    wip

--- src/SqlStreamStoreExtensions.fs
+++ src/SqlStreamStoreExtensions.fs
@@ -1,8 +1,17 @@
 namespace SqlStreamStore.FSharp
 
+open FSharp.Prelude
 open SqlStreamStore.FSharp.EventSourcing
 open SqlStreamStore.Streams
 
 [<AutoOpen>]
 module SqlStreamExtensions =
-    let foo = ""
+    let private getJsonData (streamMessage: StreamMessage) =
+        asyncResult { return! streamMessage.GetJsonData() }
+
+    let private getJsonDataAs (streamMessage: StreamMessage) =
+        asyncResult { return! streamMessage.GetJsonDataAs() }
+
+    type StreamMessage with
+        member this.GetJsonData() = getJsonData this
+        member this.GetJsonDataAs() = getJsonDataAs this
