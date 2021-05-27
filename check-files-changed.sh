4a48a3582812b588782e462b4ecca58df53ed503
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-05-27 22:36:33 +0200

    script done

---
+++ check-files-changed.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "List modified files"
+git diff --name-only HEAD^ HEAD
+echo "Check paths of modified files"
+git diff --name-only HEAD^ HEAD >files.txt
+
+sssf=false
+sssfp=false
+
+while IFS= read -r file; do
+  echo $file
+  if [[ $file == src/SqlStreamStore.FSharp/* ]]; then
+    sssf=true
+  elif [[ $file == src/SqlStreamStore.FSharp.Postgres/* ]]; then
+    sssfp=true
+  fi
+done <files.txt
+
+rm -f files.txt
+
+if [ $sssf = true ] && [ $sssfp = true ]; then
+  echo "::set-output name=run_jobs:all"
+elif [ $sssf = true ] && [ $sssfp = false ]; then
+  echo "::set-output name=run_jobs:sssf"
+elif [ $sssf = false ] && [ $sssfp = true ]; then
+  echo "::set-output name=run_jobs:sssfp"
+else
+  echo "::set-output name=run_jobs:none"
+fi
